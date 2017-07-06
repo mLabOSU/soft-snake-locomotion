b2815e868f731f621df52420fba8a103585e2e4f
Lily <lilyorthsmith@gmail.com>
2017-07-06 10:26:33 -0700

    PneumaticBoard.py now compatible with python 3.6, circular-gait uses PneumaticBoard.py class

---
+++ gait-control/PneumaticBoard.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 10:44:14 2017
+
+@author: jrs
+#python 3
+"""
+
+import serial # python -m pip install pyserial
+from time import sleep
+
+class PneumaticBoard:
+    def __init__(self, actuators=2, port='COM3', baud=9600):
+        self.pwmList = [0] * actuators
+        self.ser = serial.Serial(port, baud)
+        print ("Please wait for control board connection...")
+        sleep(5)
+        print ("Done.")
+		
+    def on(self):
+        print ("Valves on.")
+        self.ser.write(b'y')
+	
+    def off(self):
+        print ("Valves off.")
+        self.ser.write(b'n')
+		
+    def close(self):
+        self.ser.close()
+		
+    def setPWMList(self, pwmList):
+        self.pwmList = pwmList
+        self.updatePWM()
+
+    def setPWM(self, pwm, index):
+        self.pwmList[index] = pwm
+        self.updatePWM()
+		
+    def updatePWM(self):
+        command = b'v '
+        for pwm in self.pwmList:
+            if pwm <= 0:                            #if pwm < 0, then send to left side (even indices), 
+                pwm = abs(int(round(pwm)))          #if pwm > 0, then send to right side (odd indices)
+                command = command + str(abs(pwm)).encode('ascii') + b' 0'#sends pwm to left, 0 to right 
+            else: 
+                pwm = abs(int(round(pwm)))
+                command = command + b' 0 ' + str(abs(pwm)).encode('ascii') #send 0 to right, pwm to left
+		
+        self.ser.write(command)#.encode('utf-8'))
+        print(command)
+        sleep(0.5)
+        
+    def printPWM(self):
+        for pwm in self.pwmList:
+            print (pwm)
+            
+            
+"""
+		if pwm1 < 0:
+			command = b'v ' + str(abs(pwm1)).encode('ascii') + b' 0 '
+		else:
+			command = b'v 0 ' + str(abs(pwm1)).encode('ascii') + b' '
+      if pwm2 < 0:
+			command += str(abs(pwm2)).encode('ascii') + b' 0'
+		else:
+			command += b' 0 ' + str(abs(pwm2)).encode('ascii')
+"""
+
