33df690946894400feb92737349b98cb7b995619
electriclilies <lilyorthsmith@gmail.com>
2017-09-01 13:38:47 -0400

    Update boardTest.py

--- gait-control/boardTest.py
+++ gait-control/boardTest.py
@@ -1,8 +1,8 @@
 from PneumaticBoard import *
 
-board = PneumaticBoard(port='COM4')
+board = PneumaticBoard(port='COM4', actuators=8)
 board.on()
-board.setPWMList([-25, -25, -25, -25])
+board.setPWMList([25, 25, 25, 25, 25, 25, 25, 25])
 sleep(500)
 board.off()
 board.close()
